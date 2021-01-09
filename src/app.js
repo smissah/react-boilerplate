29a8e98f643532959588b7306e3660305775ab83
stephenPC <stephenmissah@outlook.com>
2021-01-09 20:51:52 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css/normalize.css";
+import "./styles/main.scss";
+ReactDOM.render(
+  <p> This is my boilder plate</p>,
+  document.getElementById("app-container")
+);
